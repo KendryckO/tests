f3affe7be8190420cb609b223961c12afd55aeb6
kendryck ontaneda <55288138+KendryckO@users.noreply.github.com>
2020-01-15 12:05:39 -0500

    Update myScript.js

--- myScript.js
+++ myScript.js
@@ -1,10 +1,15 @@
 var r;
-for( r=0; r < 31;r++) {
+for( r=0; r < 31;r++) 
+{
 document.write ("Loop number: "+ r + "<br/> <hr>" )
 } 
+
+
+
 /*
 Var e;
-for( e=30; e < 0;e--) {
+for( e=30; e < 0;e--) 
+{
 document.write ("Loop number: "+ e +"<br/>")
 }
 */
