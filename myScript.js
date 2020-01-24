85311ce436eb2b3cda5e0910fea271b1b85d1302
kendryck ontaneda <55288138+KendryckO@users.noreply.github.com>
2020-01-24 11:22:51 -0500

    Update myScript.js

--- myScript.js
+++ myScript.js
@@ -8,8 +8,19 @@ var end = document.getElementById("end").value;
   var start=
 document.getElementById("start").value;
 
+  var jump=
+ document.getElementById("jump").value;
+  
+  console.log(start);
+  console.log(typeof start);
+  console.log(typeof end);
+  console.log(typeof jump);
+  
+  start = Number(start);
+  end = Number(end);
+  jump = Number(jump);
 
-for(var i=start ; i < end; i++){
+for(var i=start ; i < end; i+=jump){
   document.write('<p>' + i)
 }
 }
